35164095826037c0744d1b8352afa51ee4775a02
U-undimk3\corey <corey@undimk3.(none)>
2012-06-23 12:46:32 -0400

    Added script to build and run the project Added test script for once I get loading working

---
+++ test.scm
@@ -0,0 +1,11 @@
+(define (factorial n)
+  (define (iter product counter max-count)
+    (if (> counter max-count)
+      product
+      (iter (+ counter product)
+            (+ counter 1)
+            max-count)))
+  (iter 1 1 n))
+
+"(factorial 5):"
+(factorial 5)
